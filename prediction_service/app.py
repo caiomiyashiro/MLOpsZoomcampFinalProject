5a4cfdaf636addd7544727e5d2b10822a129ffed
caiomiyashiro <caiomiyashiro37@gmail.com>
2024-07-02 00:52:45 +0900

    fixing action

--- prediction_service/app.py
+++ prediction_service/app.py
@@ -103,7 +103,7 @@ def predict_endpoint():
     Predict wine quality
     """
     request_input = request.get_json()
-    # print("----------------------------------------111111222", flush=True)
+    # print("----------------------------------------1111112223", flush=True)
     # print(f"{model.predict(DataFrame([request_input]))}", flush=True)
     # print("----------------------------------------", flush=True)
     # print(model.get_params, flush=True)
